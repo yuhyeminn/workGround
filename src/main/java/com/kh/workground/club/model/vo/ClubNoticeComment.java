25ecdb935639ed1e5c0ad615c28f8eed55a91838
sohhh <thgus249@gmail.com>
2020-02-11 09:56:14 +0900

    소현 학원 오전 커밋 완료

---
+++ src/main/java/com/kh/workground/club/model/vo/ClubNoticeComment.java
@@ -0,0 +1,36 @@
+package com.kh.workground.club.model.vo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ClubNoticeComment implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int clubNoticeCommentNo;
+	private int clubNoticeNo;
+	private int clubNoticeCommentLevel;
+	private int clubMemberNo;
+	private String clubNoticeCommentContent;
+	private Date clubNoticeCommentDate;
+	private String clubNoticeCommentRef;
+	private int clubNo;
+	
+	//가상컬럼
+	private String memberName;
+	private String renamedFileName;
+}
